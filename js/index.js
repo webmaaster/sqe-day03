89c6d73ef144c202dac554a35f754dccdb7ccc0b
webmaaster <wecodewetest@gmail.com>
2020-01-22 20:52:30 -0500

    Created Home page

---
+++ js/index.js
@@ -0,0 +1,9 @@
+alert('Hello world!');
+alert('How is everything?');
+alert('Are you enjoying bootcamp?');
+alert('Yes. No!');
+alert('Why?');
+alert("It's too basic for me");
+alert('I am sorry to hear that!');
+alert('No worries');
+alert('Thank you');
